042d62edb52c60cdaa093a859a7738e113785254
Tadeu Classe <tadeu.classe@uniriotec.br>
2025-11-11 13:28:24 -0300

    First version

---
+++ PidDDALib/PidDDAController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PidDDALib
+{
+    public class PidDDAController
+    {
+        // PID gain factors
+        public float Kp { get; set; }
+        public float Ki { get; set; }
+        public float Kd { get; set; }
+
+        // Setpoint (param value to reach)
+        public float Setpoint { get; set; }
+
+        // sample time
+        public float Time { get; set; }
+
+        // PID internal terms
+        private float _integral;
+        private float _previousError;
+        private bool _firstCompute = true;
+
+        // Construtor
+        public PidDDAController(float kp, float ki, float kd, float setpoint, float time)
+        {
+            Kp = kp;
+            Ki = ki;
+            Kd = kd;
+            Setpoint = setpoint;
+            Time = time;
+            Reset();
+        }
+
+        // PIC calculate
+        public float Compute(float processVariable)
+        {
+            // erros difference
+            float error = Setpoint - processVariable;
+
+            // proportional term
+            float P = Kp * error;
+
+            // integrative term
+            _integral += error * Time;
+            float I = Ki * _integral;
+
+            // derivative term
+            float derivative = 0f;
+            if (!_firstCompute)
+            {
+                derivative = (error - _previousError) / Time;
+            }
+            float D = Kd * derivative;
+
+            // update pid state
+            _previousError = error;
+            _firstCompute = false;
+
+            // final output
+            return P + I + D;
+        }
+
+        // resete pid contrller
+        public void Reset()
+        {
+            _integral = 0.00f;
+            _previousError = 0.00f;
+            _firstCompute = true;
+        }
+    }
+}
---
+++ PidDDALib/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PidDDALib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JOCCOM_UNIRIO")]
+[assembly: AssemblyProduct("PidDDALib")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8faa80a9-297e-449f-9163-14f41d320a15")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
